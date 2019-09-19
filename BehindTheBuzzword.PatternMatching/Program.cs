13cfa19d16b9e5623833eec4f8d314b34c30f581
Apostolis Daniel Apostolidis <apostolis-daniel.apostolidis@gazprom-energy.com>
2019-09-19 16:14:04 +0100

    added empty project

---
+++ BehindTheBuzzword.PatternMatching/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BehindTheBuzzword.PatternMatching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
